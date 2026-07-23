d980c464d4d6d0d350d2353c12a63d4835922878
OrtelanGabriel1 <gabrielortelan7@gmail.com>
2026-07-23 13:21:09 -0300

    Testando o Swicth no Js

---
+++ JavaScript/java_script.js
@@ -0,0 +1,14 @@
+// Swich
+
+const opcao = 3
+
+switch(opcao){
+    case 1:
+        console.log("Comprar celular")
+        break
+    case 2:
+        console.log("Comprar Tablet")
+        break
+    default: 
+    console.log ("Algo deu errado")
+}
